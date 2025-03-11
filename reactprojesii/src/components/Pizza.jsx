c9467eab999002a6ece5c5adf6999b31781d8dad
Rojin Koç <rojinin.koc@gmail.com>
2025-03-11 13:09:46 +0300

    body

--- reactprojesii/src/components/Pizza.jsx
+++ reactprojesii/src/components/Pizza.jsx
@@ -1,15 +1,14 @@
-export default function Pizza(props) {
+export default function Pizza({ pizzaObj }) {
 
-    console.log(props)
 
     return (
       <div className='col'>
         <div className="card">
-            <img src= { props.image } alt="pizza" className="card-img-top p-2 p-md-3 border-bottom" />
+            <img src= { pizzaObj.image } alt="pizza" className="card-img-top p-2 p-md-3 border-bottom" />
             <div className="card-body">
-                <h2 className="card-title "> {props.title} </h2>
-                <p className="card-description"> {props.description} </p>
-                <span className="bagde text-bg-primary bg-dark"> {props.price} TL </span>
+                <h2 className="card-title "> {pizzaObj.title} </h2>
+                <p className="card-description"> {pizzaObj.description} </p>
+                <span className="bagde text-bg-primary bg-dark"> {pizzaObj.price} TL </span>
             </div>
 
         </div>
--- reactprojesii/src/components/PizzaList.jsx
+++ reactprojesii/src/components/PizzaList.jsx
@@ -28,10 +28,13 @@ export default function PizzaList() {
       <div className='pizza-list'>
         <h2>Pizza List</h2>
         <div className='row row-cols-2 row-cols-md-3 row-cols-xl-4 g-4'>
-          <Pizza title = { pizzas[0].title }  description = { pizzas[0].description } image = { pizzas[0].image } price = { pizzas[0].price } />
-          <Pizza title = { pizzas[1].title }  description = { pizzas[1].description } image = { pizzas[1].image } price = { pizzas[1].price } />
-          <Pizza title = { pizzas[2].title }  description = { pizzas[2].description } image = { pizzas[2].image } price = { pizzas[2].price } />
 
+            {
+                pizzas.map((p, index) => (
+                    <Pizza pizzaObj = { p } key={index} />
+                ))
+            }
+          
         </div>
       </div>
     )
